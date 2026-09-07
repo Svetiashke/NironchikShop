963f190c3e914d724ba9569d8206534915abf6dd
Svetiashke <darker.coop@gmail.com>
2026-09-07 17:25:02 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -2129,8 +2129,13 @@ class APIHandler(BaseHTTPRequestHandler):
     def do_OPTIONS(self):
         self.send_response(200)
         self.send_header("Access-Control-Allow-Origin", "*")
-        self.send_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+        self.send_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, HEAD")
         self.send_header("Access-Control-Allow-Headers", "Content-Type")
+        self.end_headers()
+
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
         self.end_headers()
 
     def do_GET(self):
